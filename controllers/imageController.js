dd5bbcc9a4131abc242b2a8f2375f6892302c632
wonsik <seodnjstlr@gmail.com>
2023-12-23 21:11:09 +0900

    Fix: getImage

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -7,12 +7,4 @@ const uploadImage = asyncHandler(async (req, res) => {
    res.status(201).json(filenames);
 });
 
-//* 이미지 조회 
-const getImages = async (req, res) => {
-   const filenames = req.query.filenames.split(','); // 파일 이름들을 배열로 변환
-   const images = await Promise.all(filenames.map(filename => imageService.getImages(filename))); // 각 이미지를 병렬로 조회
-   res.set('Content-Type', 'application/json');
-   res.send(images); // 이미지 데이터를 JSON 형태로 전송
-};
-
-module.exports = { uploadImage, getImages };
+module.exports = { uploadImage };
