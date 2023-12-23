080ac538b93a0dd04422953c79eb703288eddf26
wonsik <seodnjstlr@gmail.com>
2023-12-23 21:07:47 +0900

    Fix: getImage

--- services/imageService.js
+++ services/imageService.js
@@ -14,7 +14,7 @@ const getImages = async (filename) => {
    const filePath = path.join('images/', filename);
    const data = await fs.readFile(filePath);
    const contentType = mime.contentType(path.extname(filename));
-   return { data: data.toString('base64'), contentType }; // 데이터를 base64 형태로 변환
+   return { data: data ,contentType }; // 데이터를 base64 형태로 변환
 };
 
 module.exports = { uploadImage, getImages };
